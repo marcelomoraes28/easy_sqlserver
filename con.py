a4e646ddbd3aafa81e9fc2cf572285568c637ebb
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-07-06 08:20:09 -0300

    Added singleton class in con.py

--- con.py
+++ con.py
@@ -1,5 +1,18 @@
 import pymssql
-from easy_sqlserver import Singleton
+
+"""
+Singleton class
+"""
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                                                 **kwargs)
+        return cls._instances[cls]
 
 
 class Connection(object, metaclass=Singleton):
