e8cb429021c331fbfc3bfb98f724919a2662b405
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-07-09 09:46:55 -0300

    Added filters packet

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='easy_sqlserver',
       description='A simple active record to assist in manipulating SQL Server data',
       long_description="This is a simple library to assist in manipulating SQL Server data",
-      version='0.0.3',
+      version='0.0.7',
       url='https://github.com/marcelomoraes28/easy_sqlserver',
       author='Marcelo Moraes',
       author_email='marcelomoraesjr28@gmail.com',
@@ -12,6 +12,7 @@ setup(name='easy_sqlserver',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3'
       ],
-      packages=['easy_sqlserver'],
+      packages=['easy_sqlserver',
+                'easy_sqlserver/filters'],
       install_requires='pymssql>=2.1.3',
       )
