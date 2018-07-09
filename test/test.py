afd34925ab56eb4f27bbdc6ea5d737977d312722
Marcelo dos Santos Moraes Junior <marcelo.moraes@saeinfnt009.saedigital.local>
2018-07-09 09:30:07 -0300

    Refactoring import references

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import os, sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from filters.filters import Filters
+from easy_sqlserver.filters import Filters
 
 
 class TestTypesMethods(unittest.TestCase):
